16e2be8dd21baf3988b8806635b7a715e793afa1
toolkitr <135540404+toolkitr@users.noreply.github.com>
2023-06-06 16:13:48 -0400

    Update and rename check_version.py to filer.py

--- check_version.py
+++
@@ -1,3 +0,0 @@
-def check_version(file_to_check: str) -> None:
-  file_version = file_to_check.__version__
-  return str(file_version)
---
+++ filer.py
@@ -0,0 +1,2 @@
+def filer(file_to_check: str):
+  return f"{file_to_check}__SLASH"
